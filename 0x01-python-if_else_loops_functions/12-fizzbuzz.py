5ee3ac406f05b1e788453d4385734f47bc9f3da0
Mauryn24 <maurynnyakio19@gmail.com>
2022-09-21 12:04:34 -0700

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-for a in range(1, 101):
-    if (a % 3 == 0):
-        print("Fizz", end=" ")
-    if (a % 5 == 0):
-        print("Buzz", end=" ")
-    if (a % 3 == 0) and (a % 5 == 0):
-        print("FizzBuzz", end=" ")
-    else:
-        print("{:d}".format(a), end=" ")
+def fizzbuzz():
+    for a in range(1, 101):
+        if (a % 3 == 0):
+            print("Fizz", end=" ")
+        elif (a % 5 == 0):
+            print("Buzz", end=" ")
+        elif (a % 3 == 0) and (a % 5 == 0):
+            print("FizzBuzz", end=" ")
+        else:
+            print("{:d}".format(a), end=" ")
