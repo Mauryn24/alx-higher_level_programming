1c47dd81483bb8bf4018e3b5f8322491ab4e290f
Mauryn24 <maurynnyakio19@gmail.com>
2022-09-25 13:03:26 -0700

    reverse a list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,9 @@
 def replace_in_list(my_list, idx, element):
     n = len(my_list)
     if idx < 0:
-        return None
-    elif idx > n -1:
-	return None
+        return my_list
+    elif idx > n - 1:
+        return my_list
     else:
-	my_list[idx] = element
-	return my_list
+        my_list[idx] = element
+        return my_list
