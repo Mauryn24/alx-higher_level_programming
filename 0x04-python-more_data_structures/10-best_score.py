762a36c33d604c9bebb9e9d47e0b8443c2a01a16
Mauryn24 <maurynnyakio19@gmail.com>
2022-09-28 02:58:27 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@ def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
     biggest = max(a_dictionary.values())
-    for key, value in my_dict.items():
+    for key, value in a_dictionary.items():
         if value is biggest:
             return key
