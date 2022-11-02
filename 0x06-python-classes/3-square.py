b72c3db7e91e1b5b9257ee97fda4efff365cd9ba
Mauryn24 <maurynnyakio19@gmail.com>
2022-11-02 08:33:25 +0300

    area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""define a class"""
+
+
+class Square:
+    """
+    you are not allowed to import a module
+    """
+    def __init__(self, size=0):
+        """
+        Instantiation with optional size
+        size must be an integer,
+        otherwise raise a TypeError exception with the message
+        size must be an integer
+        if size is less than 0,
+        raise a ValueError exception with the
+        message size must be >= 0
+        private instance attribute
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        def area(self):
+            """Public instance method tha returns square area"""
+            return self.__size ** 2
