4273aec95aa0b81c45c39deee471e295150b6fc5
Mauryn24 <maurynnyakio19@gmail.com>
2022-11-02 07:41:32 +0300

    private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+define a class
+"""
+
+
+class  Square:
+    """
+    keeping the size attribute private
+    Instantiation with size
+    You are not allowed to import any module
+    """
+    def __init__(self, size):
+        """
+        private attribute
+        """
+        self.size = size
